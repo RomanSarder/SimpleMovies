42fc828f230573d9ca1643b15a2b39564ad38dfe
RomanSarder <roman.sarder@yandex.ru>
2017-09-12 04:09:35 +0600

    Initial setup

---
+++ src/store/reducer/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+
+// Use ES6 object literal shorthand syntax to define the object shape
+const reducer = combineReducers({
+   
+});
+
+export default reducer;
